e2ea7ffac4278a81973c04954f16c95a92010ffc
sofvanh <23138848+sofvanh@users.noreply.github.com>
2024-11-24 15:59:09 +0000

    Add TODO

--- src/components/MarkdownRenderer.tsx
+++ src/components/MarkdownRenderer.tsx
@@ -58,7 +58,7 @@ export function MarkdownRenderer({ markdown, isModified = false }: MarkdownRende
       {isModified && (
         <div className="border border-rose-300 bg-rose-50 text-rose-700 p-4 my-4 rounded flex justify-between items-center">
           <span>Please note that this text has been personalized by AI and might contain mistakes.</span>
-          <button className="text-rose-500 font-bold">x</button>
+          <button className="text-rose-500 font-bold">x</button> {/* TODO Make it so that this can actually be closed. */}
         </div>
       )}
       <div className={`prose prose-lg mt-8 ${isModified ? '!font-ai' : '!font-handwritten text-xl'}`}>
