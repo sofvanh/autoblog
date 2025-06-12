c04c3e4f85f4dcc3cad59e7deaa29d5283e62f32
sofvanh <23138848+sofvanh@users.noreply.github.com>
2025-06-12 12:19:20 +0100

    Update footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,12 +2,9 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="py-4 bg-gradient-to-r from-green-100 to-teal-200 shadow-sm text-emerald-800">
-      <div className="px-8 flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 text-sm">
-        <div className="flex items-center space-x-2">
-          <Link href="https://github.com/sofvanh/autoblog">GitHub</Link>
-        </div>
-      </div>
+    <div className="px-8 py-8 bg-gradient-to-r from-green-100 to-teal-200 shadow-sm text-emerald-800 flex justify-center gap-8">
+      <Link className="text-sm" href="https://github.com/sofvanh/autoblog">GitHub</Link>
+      <Link className="text-sm" href="https://sofiavanhanen.com">Created by Sofia Vanhanen</Link>
     </div >
   );
 }
