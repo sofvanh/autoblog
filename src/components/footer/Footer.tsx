54e83945571d57b2aa299d18b7a151b7df73ab06
sofvanh <23138848+sofvanh@users.noreply.github.com>
2025-07-02 16:48:35 +0300

    Fix link

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="px-8 py-8 bg-gradient-to-r from-green-100 to-teal-200 shadow-sm text-emerald-800 flex justify-center gap-8">
       <Link className="text-sm" href="https://github.com/sofvanh/autoblog">GitHub</Link>
-      <Link className="text-sm" href="https://sofiavanhanen.com">Created by Sofia Vanhanen</Link>
+      <Link className="text-sm" href="https://sofiavanhanen.fi">Created by Sofia Vanhanen</Link>
     </div >
   );
 }
